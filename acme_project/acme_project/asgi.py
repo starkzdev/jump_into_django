932c0ca2d250bd53d9973e040b3e5cfff3074619
Sergey Tsepilov <starkzdev@mail.ru>
2024-04-08 21:00:16 +0500

    asgi

---
+++ acme_project/acme_project/asgi.py
@@ -0,0 +1,9 @@
+"""ASGI конфигурация для проекта acme_project."""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "acme_project.settings")
+
+application = get_asgi_application()
